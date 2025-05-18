62e5ee2adea51a455ccbae9a6968972e663b5674
nooh <nooh.saeed@gmail.com>
2025-05-18 13:08:59 +0300

    finish notes  bottom sheet

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/views/widgets/custom_button.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -9,20 +11,27 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(height: 32,),
-          CustomTextField(
-            hint:"Title",
-            ),
-
-            SizedBox(height: 16,),
-
-          CustomTextField(
-            hint:"Content",
-            maxLines: 5,
-            ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 32,),
+            CustomTextField(
+              hint:"Title",
+              ),
+        
+              SizedBox(height: 16,),
+        
+            CustomTextField(
+              hint:"Content",
+              maxLines: 5,
+              ),
+      
+            SizedBox(height: 40,),
+              CustomButton(),
+         
+            SizedBox(height: 32,),
+          ],
+        ),
       ),
     );
   }
